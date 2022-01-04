5eb6244a6f702abce13de865e6d94087650843af
Peter Ruibal <ruibalp@gmail.com>
2022-01-04 15:56:16 -0800

    build updates (including install to dist/)

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
-cmake -D BUILD_SHARED_LIBS=OFF -A Win32 .
+rm -rf CMakeCache.txt  CMakeFiles  Debug/ Release/ *.dir/ dist/
+cmake -D BUILD_SHARED_LIBS=OFF .
+mkdir dist
 cmake --build . --config Release
+cmake --install . --config Release --prefix dist
